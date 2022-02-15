6fd62ff0c9a5ee328109419e054f9a0ef72db0f8
Kristell L <klansonneur@hotmail.com>
2022-02-15 10:57:59 +0100

    add front pages

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -28,12 +28,12 @@ exports.signup = (req, res, next) => {
     User.findOne({name: name})
       .then(user => {
         if (!user) {
-          return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+          return res.status(401).json({ error: 'Utilisateur non trouvé !' });//changer le message pour qq de plus générique
         }
         bcrypt.compare(req.body.password, user.password)
           .then(valid => {
             if (!valid) {
-              return res.status(401).json({ error: 'Mot de passe incorrect !' });
+              return res.status(401).json({ error: 'Mot de passe incorrect !' });//changer le message pour qq de plus générique
             }
             res.status(200).json({
               userId: user._id,
